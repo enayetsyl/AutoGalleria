6c18968f2f3fb893b2b3b25a910fc2a8502c8a79
Enayet <enayetflweb@gamil.com>
2023-10-18 18:40:11 +0600

    Car component updated

--- src/Components/Car.jsx
+++ src/Components/Car.jsx
@@ -1,8 +1,17 @@
 
-const Car = () => {
+const Car = ({car}) => {
+  const {image, name} = car;
+  console.log(car)
   return (
     <div>
-      
+      <div className="bg-[#ffffff] border border-solid border-[#dddddd] rounded-xl">
+        <img src={image} alt="" className="rounded-t-xl h-[320px] w-full"/>
+        <div className="p-5">
+        <h1 className="font-mont text-[#333333] font-bold text-3xl pb-3">{name}</h1>
+        <hr />
+        <h1 className="pt-3 flex justify-around font-mont text-2xl font-bold text-[#333333]"> <span></span> </h1>
+        </div>
+      </div>
     </div>
   );
 };
